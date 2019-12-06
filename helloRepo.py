3392c13ff4c9edda5e85df49a4cd1f090ab00d8e
skylarwh <BlahBLah5!>
2019-12-06 16:01:25 -0700

    more testing

--- helloRepo.py
+++ helloRepo.py
@@ -1,4 +1,6 @@
 # Sample Program just to test git
+#------CHANGELOG------------
+# 12/6/19: Added new out put for tests
 
 print('This is a python program in my repo')
-
+print('This has now been updated for git test')
